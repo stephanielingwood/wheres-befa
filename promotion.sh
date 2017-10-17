69d54d28317eaf88b3839569a4722c14b64ed50b
stephanielingwood <stephanielingwood@gmail.com>
2017-10-17 10:38:09 -0700

    Remove quotes from env vars

--- promotion.sh
+++ promotion.sh
@@ -1,9 +1,16 @@
 apt-get update
 apt-get install jq
-npm install -g serverless
+npm install -g serverless -s
 
-export OPEN_SKY_USERNAME=${$(aws ssm get-parameters --names OPEN_SKY_USERNAME --with-decryption | jq '.Parameters | .[0] | .Value')//\"}
-export OPEN_SKY_PASSWORD=${$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')//\"}
+export OPEN_SKY_USERNAME=$(aws ssm get-parameters --names OPEN_SKY_USERNAME --with-decryption | jq '.Parameters | .[0] | .Value')
+export OPEN_SKY_USERNAME=${OPEN_SKY_USERNAME//\"}
+export OPEN_SKY_PASSWORD=$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')
+export OPEN_SKY_PASSWORD=${OPEN_SKY_PASSWORD//\"}
+
+# todo: get rid of quotes
+# eitehr use javascript
+# or re-export
+# then, update buildspec.yml
 
 echo $OPEN_SKY_USERNAME
 echo $API_URL
