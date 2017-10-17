fd8d63a8a3e5e0f84e7e92ce93cbd864b356a43f
stephanielingwood <stephanielingwood@gmail.com>
2017-10-17 11:01:01 -0700

    Update env vars in buildspec; remove comments

--- promotion.sh
+++ promotion.sh
@@ -7,14 +7,6 @@ export OPEN_SKY_USERNAME=${OPEN_SKY_USERNAME//\"}
 export OPEN_SKY_PASSWORD=$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')
 export OPEN_SKY_PASSWORD=${OPEN_SKY_PASSWORD//\"}
 
-# todo: get rid of quotes
-# eitehr use javascript
-# or re-export
-# then, update buildspec.yml
-
-echo $OPEN_SKY_USERNAME
-echo $API_URL
-
 cd airplanes
 npm install -s
 serverless deploy -f getAll -s prod -r us-east-1 -v
