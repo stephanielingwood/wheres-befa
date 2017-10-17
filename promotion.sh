fdbfdde6004b6cadc2660689573210a888a05fd2
stephanielingwood <stephanielingwood@gmail.com>
2017-10-16 20:42:24 -0700

    Add package.json to artifacts

--- promotion.sh
+++ promotion.sh
@@ -1,9 +1,10 @@
 npm install -g serverless
-npm install
 
 cd airplanes
+npm install
 serverless deploy -f getAll -s prod -r us-east-1 -v
 serverless deploy -f post -s prod -r us-east-1 -v
 
 cd ../airplanesCron
+npm install
 serverless deploy -f cron -s prod -r us-east-1 -v
