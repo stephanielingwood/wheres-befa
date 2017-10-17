384513391886d07b4bcacf715a7bc5d5336f42db
stephanielingwood <stephanielingwood@gmail.com>
2017-10-17 01:10:48 -0700

    Remove double quotes from env vars

--- promotion.sh
+++ promotion.sh
@@ -2,8 +2,8 @@ apt-get update
 apt-get install jq
 npm install -g serverless
 
-export OPEN_SKY_USERNAME=$(aws ssm get-parameters --names OPEN_SKY_USERNAME --with-decryption | jq '.Parameters | .[0] | .Value')
-export OPEN_SKY_PASSWORD=$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')
+export OPEN_SKY_USERNAME=${$(aws ssm get-parameters --names OPEN_SKY_USERNAME --with-decryption | jq '.Parameters | .[0] | .Value')//\"}
+export OPEN_SKY_PASSWORD=${$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')//\"}
 
 echo $OPEN_SKY_USERNAME
 echo $API_URL
