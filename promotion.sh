4f1e30918ad81c81663788e7dc374e77f5621a7c
stephanielingwood <stephanielingwood@gmail.com>
2017-10-17 00:03:22 -0700

    Get secrets from SSM

--- promotion.sh
+++ promotion.sh
@@ -1,4 +1,7 @@
+apt-get install jq
 npm install -g serverless
+
+export OPEN_SKY_PASSWORD=$(aws ssm get-parameters --names OPEN_SKY_PASSWORD --with-decryption | jq '.Parameters | .[0] | .Value')
 
 cd airplanes
 npm install
@@ -8,3 +11,4 @@ serverless deploy -f post -s prod -r us-east-1 -v
 cd ../airplanesCron
 npm install
 serverless deploy -f cron -s prod -r us-east-1 -v
+
